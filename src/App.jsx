50a72f869af0236a03e3b8e49c845583977f1028
Md Royet <rroyet8@gmail.com>
2026-04-12 21:51:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Simpleform from "./Components/Simpleform/Simpleform";
+
+function App() {
+  return (
+    <>
+      <div id="center">Explore React Form</div>
+      <Simpleform></Simpleform>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Simpleform/Simpleform.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Simpleform = () => {
+  return <div></div>;
+};
+
+export default Simpleform;
